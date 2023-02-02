e883b76eaff714b58238999cf1f5ceadcfc4b22f
Christian <onyekachristian14@gmail.com>
2023-02-02 17:33:09 +0100

    added provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
 import 'package:shopapp/screens/product_overview_screen.dart';
+import './providers/products.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,23 +15,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-            .copyWith(secondary: Colors.deepOrange),
-        textTheme: TextTheme(
-          titleMedium: GoogleFonts.roboto(),
-          titleLarge: GoogleFonts.roboto(),
-          bodyLarge: GoogleFonts.roboto(),
-          bodyMedium: GoogleFonts.roboto(),
-          bodySmall: GoogleFonts.roboto(),
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          textTheme: TextTheme(
+            titleMedium: GoogleFonts.roboto(),
+            titleLarge: GoogleFonts.roboto(),
+            bodyLarge: GoogleFonts.roboto(),
+            bodyMedium: GoogleFonts.roboto(),
+            bodySmall: GoogleFonts.roboto(),
+          ),
         ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
