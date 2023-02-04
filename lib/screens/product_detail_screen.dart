457591bd9bfa5a8b4e5ffbe1cade23799fb42e0d
Christian <onyekachristian14@gmail.com>
2023-02-04 21:08:12 +0100

    added comment for route name

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
+  //route name
   static const routeName = "product_detail";
   const ProductDetailScreen();
 
