6ea1e20ff26b6673f89b66f2b4d2592eb9b89808
Christian <onyekachristian14@gmail.com>
2023-03-07 01:28:16 +0100

    added multiline input and dispose focusNode object to avoid memeory leak

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,7 +8,17 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
-  final _priceNode = FocusNode();
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,14 +34,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 decoration: const InputDecoration(labelText: "Title"),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_priceNode);
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: "Price"),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
-                focusNode: _priceNode,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: ((_) =>
+                    FocusScope.of(context).requestFocus(_descriptionFocusNode)),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "Description"),
+                textInputAction: TextInputAction.done,
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
               ),
             ],
           ),
